4c3cbebbb7a7f64f499075bd73ebb080fdc54ed3
Malte Böhme <malte.boehme@affiliprint.com>
2019-01-07 15:33:41 +0100

    admin: add search_fields and filter for NoticeSetting, add 'id' to list_display for NoticeType

--- pinax/notifications/admin.py
+++ pinax/notifications/admin.py
@@ -4,11 +4,13 @@ from .models import NoticeQueueBatch, NoticeSetting, NoticeType
 
 
 class NoticeTypeAdmin(admin.ModelAdmin):
-    list_display = ["label", "display", "description", "default"]
+    list_display = ["id", "label", "display", "description", "default"]
 
 
 class NoticeSettingAdmin(admin.ModelAdmin):
     list_display = ["id", "user", "notice_type", "medium", "scoping", "send"]
+    list_filter = ["notice_type", "medium"]
+    search_fields = ["user__email", "user__username"]
 
 
 admin.site.register(NoticeQueueBatch)
